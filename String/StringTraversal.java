f8039a35b615a43fc310e8ce16f40b335633a0c7
Loukikareddy08 <loukikareddy14@gmail.com>
2026-08-17 09:58:07 +0530

    StringTraversal

---
+++ String/StringTraversal.java
@@ -0,0 +1,9 @@
+public class StringTraversal {
+    public static void main(String[] args) {
+        String str = "Loukika Reddy";
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
+}
